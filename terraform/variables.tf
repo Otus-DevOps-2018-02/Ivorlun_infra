c302c567e12f214ed4f54a9b24217870a7dae738
Ivan Staroselskiy <ivorlun@mail.ru>
2018-04-22 20:56:41 +0300

    prepare for difficult tasks

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,37 +4,5 @@ variable project {
 
 variable region {
   description = "Region"
-  default     = "europe-west4"
-}
-
-variable zone {
-  description = "Zone"
-  default     = "europe-west4-b"
-}
-
-variable public_key_path {
-  description = "Path to the public key used for ssh access"
-}
-
-variable private_key_path {
-  description = "Path to the private key used for ssh access"
-}
-
-variable disk_image {
-  description = "Disk image"
-}
-
-variable app_disk_image {
-  description = "Disk image for reddit app"
-  default     = "reddit-app-base"
-}
-
-variable db_disk_image {
-  description = "Disk image for reddit db"
-  default     = "reddit-db-base"
-}
-
-variable num_of_apps {
-  description = "number of instances"
-  default     = 1
+  default     = "europe-west1"
 }
