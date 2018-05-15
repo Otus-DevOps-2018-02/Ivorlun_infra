62c07f319d60a3a135e0dca55b5048c2af123583
Ivan Staroselskiy <ivorlun@mail.ru>
2018-05-15 12:01:27 +0300

    terraform tests fix tfvars

--- tests/terraform_tests.sh
+++ tests/terraform_tests.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 set -e
-cd ../terraform/stage && terraform init -backend=false && terraform validate && tflint
-cd ../prod && terraform init -backend=false && terraform validate && tflint
+cd ../terraform/stage && mv terraform.tfvars.example terraform.tfvars && \
+terraform init -backend=false && terraform validate && tflint
+cd ../prod && mv terraform.tfvars.example terraform.tfvars && \
+terraform init -backend=false && terraform validate && tflint
